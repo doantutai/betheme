293b324ad6a859970899051979ba158ddf84d572
Doan Tu Tai <tai.doan@sstechvn.com>
2018-05-08 09:49:46 +0700

    be theme

---
+++ functions/admin/templates/notice-register.php
@@ -0,0 +1,4 @@
+<div class="notice notice-info">
+	<p><strong>Welcome to BeTheme</strong></p>
+	<p>Theme nulled by <a target="_blank" href="https://namlee.net">NamLee.Net</a> & <a target="_blank" href="https://themegiare.net">ThemeGiaRe.Net</a></p>
+</div>
